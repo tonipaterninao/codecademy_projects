2e14a60d3a0b1b8f0ea82152616e7cf4b42e4056
Toni Paternina <git@tonipaternina.com>
2021-01-25 14:37:26 +0100

    change from previous incoherent messages to whacky non-sensical cookie-fortne-style inspirational quotes.

--- 01-Portfolio_MixedMessages/main.js
+++ 01-Portfolio_MixedMessages/main.js
@@ -2,93 +2,80 @@
  * First Portfolio project
  * Mixed Messages: 
  * Returns a randomized inspirational quote everytime it runs.
- * 
- * Source of the inspirational quotes: http://www.positivityblog.com/quotes-on-education/
+ * The quote components were inspired by the inspirational quotes in: 
+ * http://www.positivityblog.com/quotes-on-education/
  */
 
 // Use fs to read files as input
 const fs = require('fs');
-const { capitalize } = require('lodash');
 // Use lodash to manipulate string
 const _ = require('lodash');
 
-
-// The raw text from the quotes
-let quoteText = fs.readFileSync(__dirname + '/QUOTES.txt', 'utf8');
-
-// Create an array of quotes and authors
-let textArray = quoteText.split('\n').filter(s => s !== '');
-
-// Even indexes correspond to quotes
-// Separate them from authors
-let quotesArray = textArray.filter((s, index) => !(index % 2));
-let authorArray = textArray.filter((s, index) => index % 2);
-
-// Function to convert strings to sentence case
-function sentenceCase(string) {
-    // capitalize the first letter after a period
-    let stringArr = string.split('.');
-    // remove white spaces and capitalize sentences
-    capSentence = stringArr.map(_.trim)
-    capSentence = capSentence.map(_.capitalize)
-    // Join elements of array into single string (and remove trailing whitespace)
-    capSentence = _.trim(capSentence.join('. '));
-
-    // replace lower case i for I (the pronoun)
-    const res = _.replace(capSentence, /(?<=\s)i(?=\s|,|\.)/, 'I');
-
-    return res;
+// Create an object to store the text from each component of the message
+const rawText = {
+    start: '',
+    verbs: '',
+    nouns: ''
 }
 
-// Convert all quotes to sentence case (as opposed to upper case)
-quotesArray = quotesArray.map(sentenceCase);
+// Read the raw text from each component
+rawText.start = fs.readFileSync(__dirname + '/SENTENCE_START.txt', 'utf8');
+rawText.verbs = fs.readFileSync(__dirname + '/VERBS.txt', 'utf8');
+rawText.nouns = fs.readFileSync(__dirname + '/NOUNS.txt', 'utf8');
 
-// Convert all authors to title case
-authorArray = authorArray.map(_.toLower).map(_.startCase);
 
-/**
- * I realized I misunderstood the assignment, but I'm gonna catch up
- * the idea is to generate randomized messages, not to pick a message at random and print it (as I initially thought)
- * Now that I have all these quotes, I'm just gonna select 3 quotes at random
- * then pick two or three words from each
- * and stitch them together the best I can
- */
+// Create an object to store the arrays of each component
+const arrays = {
+    start: '',
+    verbs: '',
+    nouns: ''
+}
 
-// Parts of the message
-let firstPart = '';
-let secondPart = '';
-let thirdPart = '';
+for (let key in rawText) {
+    arrays[key] = rawText[key].split('\n').filter(s => s !== '');
+}
 
-// Select 2 first words
-const firstIndex = Math.floor(Math.random() * quotesArray.length); 
-firstPart = _.words(quotesArray[firstIndex]).slice(0, 3);
+// Turn all elements of each array to lower case
+for (let key in arrays) {
+    arrays[key] = arrays[key].map(_.toLower);
+}
 
-// Select 3 last words
-// make sure we select a different quote
-let thirdIndex;
+// Capitalize the first letter of the "start" phrase
+arrays.start = arrays.start.map(_.capitalize);
+
+
+// Select random indexes for each component
+
+// the beginning of the sentence
+let startIndex = Math.floor(Math.random() * arrays.start.length);
+
+// the first verb
+let verb1Index = Math.floor(Math.random() * arrays.verbs.length);
+// the second verb, make sure it's not the same as first verb
+let verb2Index;
 do {
-    thirdIndex = Math.floor(Math.random() * quotesArray.length);
-} while (thirdIndex === firstIndex);
+    verb2Index = Math.floor(Math.random() * arrays.verbs.length);
+} while (verb2Index === verb1Index);
 
-thirdPart = _.words(quotesArray[thirdIndex]).slice(-3);
-
-// The middle words 
-let secondIndex;
+// the frist noun
+let noun1Index = Math.floor(Math.random() * arrays.nouns.length);
+// the second noun, make sure it's not the same as first noun
+let noun2Index;
 do {
-    secondIndex = Math.floor(Math.random() * quotesArray.length);
-} while (secondIndex === firstIndex || secondIndex === thirdIndex);
+    noun2Index = Math.floor(Math.random() * arrays.nouns.length);
+} while (noun1Index === noun2Index)
 
-// Find the middle of the quote
-let middleQuote = _.words(quotesArray[secondIndex]);
-const middle = Math.floor(middleQuote.length / 2);
+// Pick each element
+const theStart = arrays.start[startIndex];
+const theVerb1 = arrays.verbs[verb1Index];
+const theVerb2 = arrays.verbs[verb2Index];
+const theNoun1 = arrays.nouns[noun1Index];
+const theNoun2 = arrays.nouns[noun2Index];
 
-// Get the surrounding words of the middle of the quote
-secondPart = middleQuote.slice(middle-2, middle+1);
+// Assemble the message
+const theMessage = `${theStart} ${theVerb1} ${theNoun1}. You ${theVerb2} ${theNoun2}.`
 
-// Join the result
-const res = `${firstPart.join(' ')} ${secondPart.join(' ')}, ${thirdPart.join(' ')}.`
-
-console.log(res);
+console.log(theMessage);
 
 
  
